3d345122e9dcce1cb67d923f9c2a45c13a089394
Devon Lewis <devonlewis808@gmail.com>
2020-09-05 16:21:14 -0600

    Basic config of our testing suite

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import EnzymeAdapter from 'enzyme';
+import Enzyme from 'enzyme';
+
+Enzyme.configure(
+    {
+        adapter: new EnzymeAdapter,
+        disableLifecycleMethods: true
+    }
+)
